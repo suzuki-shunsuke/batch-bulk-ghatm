7a7739230b5b468744cc81300b480469d87a6991
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-06-30 08:32:24 +0900

    update

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ body="https://github.com/suzuki-shunsuke/batch-bulk-ghatm/issues/1"
 export GITHUB_TOKEN=$(gh auth token)
 
 multi-gitter run ./ghatm-set.sh \
+  --config config.yaml \
   -O "$org" \
   -t "ci: set timeout-minutes using ghatm" \
   -b "$body" \
