a4f2590e2827440a7c962d306bdc27e8aa2326a7
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-06-30 09:57:43 +0900

    update

--- merge.sh
+++ merge.sh
@@ -6,6 +6,15 @@ org=$1
 
 export GITHUB_TOKEN=$(gh auth token)
 
+if [ "$org" = suzuki-shunsuke ]; then
+	multi-gitter merge \
+		-U "$org" \
+		-B ci-set-timeout-minutes-by-ghatm \
+		--skip-forks
+	exit 0
+fi
+
 multi-gitter merge \
-  -O "$org" \
-  -B ci-set-timeout-minutes-by-ghatm
+	-O "$org" \
+	-B ci-set-timeout-minutes-by-ghatm \
+	--skip-forks
--- run.sh
+++ run.sh
@@ -8,9 +8,20 @@ body="https://github.com/suzuki-shunsuke/batch-bulk-ghatm/issues/1"
 
 export GITHUB_TOKEN=$(gh auth token)
 
+if [ "$org" = suzuki-shunsuke ]; then
+	multi-gitter run ./ghatm-set.sh \
+		--config config.yaml \
+		-U "$org" \
+		-t "ci: set timeout-minutes using ghatm" \
+		-b "$body" \
+		--skip-forks \
+		-B ci-set-timeout-minutes-by-ghatm
+	exit 0
+fi
+
 multi-gitter run ./ghatm-set.sh \
-  --config config.yaml \
-  -O "$org" \
-  -t "ci: set timeout-minutes using ghatm" \
-  -b "$body" \
-  -B ci-set-timeout-minutes-by-ghatm
+	--config config.yaml \
+	-O "$org" \
+	-t "ci: set timeout-minutes using ghatm" \
+	-b "$body" \
+	-B ci-set-timeout-minutes-by-ghatm
